81a048f4830dcdb203d8152fa89cdb6101adc4b9
Caleb Figgers <cdfiggers@gmail.com>
2022-02-20 16:19:45 -0600

    Update setup-script

--- setup-script.clj
+++ setup-script.clj
@@ -4,11 +4,15 @@
 ;; REPL such as Calva in VS Code
 (do (require '[clojure.java.io :as io])
     (for [n (range 1 31)]
-      (do (io/make-parents (str "./" (format "%02d" n) "/sample.txt"))
-          (spit (str "./" (format "%02d" n) "/sample.txt") "" :append true)
-          (spit (str "./" (format "%02d" n) "/input.txt") "" :append true)
-          (spit (str "./" (format "%02d" n) "/day-" (format "%02d" n) ".clj") "" :append true)
-          (when (empty? (slurp (str "./" (format "%02d" n) "/day-" (format "%02d" n) ".clj")))
-            (spit (str "./" (format "%02d" n) "/day-" (format "%02d" n) ".clj")
-                  (str "(ns aoc-2021.day-" (format "%02d" n) "\n"
-                       "  (:require [clojure.string :as s]))"))))))
+      (let [thisnum (format "%02d" n)
+            thissample (str "./" thisnum "/sample.txt")
+            thisinput (str "./" thisnum "/input.txt")
+            thisfile (str "./" thisnum "/day-" thisnum ".clj")]
+        (io/make-parents thissample)
+        (spit thissample "" :append true)
+        (spit thisinput "" :append true)
+        (spit thisfile "" :append true)
+        (when (empty? (slurp thisfile))
+          (spit thisfile
+                (str "(ns aoc-2021.day-" thisnum "\n"
+                     "  (:require [clojure.string :as s]))"))))))
