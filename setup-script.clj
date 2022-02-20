6ea623584af7ca6f90700451a4df9e44c2f65eb4
Caleb Figgers <cdfiggers@gmail.com>
2022-02-20 16:16:42 -0600

    Updates to setup-script

--- setup-script.clj
+++ setup-script.clj
@@ -1,9 +1,18 @@
 #!/usr/bin/env bb
 
-(for [n (range 1 31)]
-  (let [thisnum (format "%02d" n)
-        thisfile (str "./" thisnum "/day-" thisnum ".clj")]
-    (when (= "" (slurp thisfile)) 
-      (spit thisfile 
-            (str "(ns aoc-2021.day-" thisnum "\n"  
-                 "  (:require [clojure.string :as s]))")))))
+;; Run this function either using Babashka or an integrated
+;; REPL such as Calva in VS Code
+(do (require '[clojure.java.io :as io])
+    (for [n (range 1 31)]
+      (let [thisnum (format "%02d" n)
+            thissample (str "./" thisnum "/sample.txt")
+            thisinput (str "./" thisnum "/input.txt")
+            thisfile (str "./" thisnum "/day-" thisnum ".clj")]
+        (io/make-parents thissample)
+        (spit thissample "" :append true)
+        (spit thisinput "" :append true)
+        (spit thisfile "" :append true)
+        (when (empty? (slurp thisfile))
+          (spit thisfile
+                (str "(ns aoc-2021.day-" thisnum "\n"
+                     "  (:require [clojure.string :as s]))"))))))
