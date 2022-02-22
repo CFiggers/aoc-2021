67d9c10501bd9a1daecd184b52d71756d49713c3
Caleb Figgers <cdfiggers@gmail.com>
2022-02-22 17:59:48 -0600

    Solved 01

---
+++ 01/day_01.clj
@@ -0,0 +1,22 @@
+(ns day-01
+  (:require [clojure.string :as s]))
+
+(def input 
+  (map #(Integer. %) 
+       (s/split-lines (slurp "./01/input.txt"))))
+
+(defn part1 [in]
+  (->> in
+    (partition 2 1)
+    (map #(if (< (first %) (second %)) 1 0))
+    (reduce +)))
+
+(defn part2 [in]
+  (->> in
+    (partition 3 1)
+    (map #(reduce + %))
+    part1))
+
+
+(defn print1 [] (println (str " - Part 1 Answer: " (part1 input))))
+(defn print2 [] (println (str " - Part 2 Answer: " (part2 input)))) 
