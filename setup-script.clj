e686ebc6153cddfda31eb9873eca1423570b6c25
Caleb Figgers <cdfiggers@gmail.com>
2022-02-22 18:01:19 -0600

    Updated setup script

--- setup-script.clj
+++ setup-script.clj
@@ -7,12 +7,21 @@
       (let [thisnum (format "%02d" n)
             thissample (str "./" thisnum "/sample.txt")
             thisinput (str "./" thisnum "/input.txt")
-            thisfile (str "./" thisnum "/day-" thisnum ".clj")]
+            thisfile (str "./" thisnum "/day_" thisnum ".clj")]
         (io/make-parents thissample)
         (spit thissample "" :append true)
         (spit thisinput "" :append true)
         (spit thisfile "" :append true)
         (when (empty? (slurp thisfile))
           (spit thisfile
-                (str "(ns aoc-2021.day-" thisnum "\n"
-                     "  (:require [clojure.string :as s]))"))))))
+                (str "(ns day-" thisnum "\n"
+                     "  (:require [clojure.string :as s]))" "\n"
+                     "" "\n"
+                     "(s/split-lines (slurp \"./" thisnum "/sample.txt\"))" "\n"
+                     "" "\n"
+                     "(defn part1 [] \"Not implemented yet\")"
+                     "" "\n"
+                     "(defn part2 [] \"Not implemented yet\")" "\n"
+                     "" "\n"
+                     "(defn print1 [] (println \"- Part 1 Answer: Not implemented yet\"))" "\n"
+                     "(defn print2 [] (println \"- Part 2 Answer: Not implemented yet\"))"))))))
