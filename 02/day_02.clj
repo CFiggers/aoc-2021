84a309850b4624dd6154a862f81cbb53840161ac
Caleb Figgers <cdfiggers@gmail.com>
2022-02-22 22:06:25 -0600

    Solved 02

--- 02/day_02.clj
+++ 02/day_02.clj
@@ -28,5 +28,5 @@
   (let [result (reduce run2 [0 0 0] in)]
     (* (first result) (second result))))
 
-(defn print1 [] (println (str " - Part 1 Answer: " (part1 input))))
-(defn print2 [] (println (str " - Part 2 Answer: " (part2 input))))
+(defn ans1 [] (str (part1 input)))
+(defn ans2 [] (str (part2 input)))
